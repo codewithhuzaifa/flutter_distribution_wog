eff2878fd79dfed3a575be87729d498c773ee1f9
codewithhuzaifa <huzaifaarab2002@gmail.com>
2024-10-29 15:23:04 +0500

    Pack k

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo testig',
+      title: 'Flutter Demo testig   kk kk kk',
       theme: ThemeData(
- 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
